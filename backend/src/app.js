57a0291a2ed4c3af13604623a1de1026f0fe1b6c
Leonardo Pernomian <leonardopernomian@hotmail.com>
2026-01-19 15:57:55 -0300

    feat: adicionando rotas minimas para test dar green

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,4 +3,15 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const assets = [];
+
+app.post("/assets", (req, res) => {
+  assets.push(req.body);
+  res.status(201).send();
+});
+
+app.get("/assets", (req, res) => {
+  res.status(200).json(assets);
+});
+
 module.exports = app;
